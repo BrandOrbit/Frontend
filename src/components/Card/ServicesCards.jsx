0754a2095935e19b69fc7b13034fbca74e3f199f
BrandOrbit <support@brandorbitmedia.in>
2025-08-19 01:03:02 +0530

    Added Services card and Update Home with second section

---
+++ src/components/Card/ServicesCards.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+// NativeAdCard.jsx
+export default function ServicesCards({ item }) {
+  return (
+    <div className="bg-white rounded-xl p-8 transition-all duration-400 text-center  mx-auto border border-[#f2f2f4] 
+      hover:shadow-2xl hover:border-transparent group">
+      <div className="flex justify-center mb-4">
+        <div className="rounded-full w-20 h-20 flex items-center justify-center ">
+         
+           <img src="/logo.png" alt="image " />
+        </div>
+      </div>
+      <h3 className="text-2xl font-semibold mb-2 text-gray-900">{item.title}</h3>
+      <p className="text-gray-500 mb-6">{item.description}</p>
+      <button
+        className="px-8 py-2 rounded border-2 border-[#fd2e6a] text-[#fd2e6a] font-semibold transition 
+        duration-1000 group-hover:bg-gradient-to-r group-hover:from-[#fd2e6a] group-hover:to-[#ff5c5c] 
+        group-hover:text-white group-hover:border-transparent"
+      >
+        Read More
+      </button>
+    </div>
+  );
+}
+
+
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,31 +1,49 @@
 import React, { useState } from 'react'
+import ServicesCards from '../Card/ServicesCards';
 
 const Home = () => {
     const [hovered, setHovered] = useState(false);
+
+    const itemInfo=[
+        {
+
+  title: "Performance Marketing",
+  description: "Maximizing ROI through data-driven and results-oriented strategies."
+  
+},
+        {
+            title:"Mobile Marketing",
+            description:"Targeting mobile users with personalized and impactful campaigns."
+        },
+        {
+            title:"Native Ads",
+            description:"Integrating ads seamlessly into content for enhanced user experience."
+        }
+    ]
   return (
-    <div className="flex justify-center">
-      <section className="bg-white pt-12 pb-20 w-full max-w-6xl">
+    <div className="flex flex-col items-center w-full">
+      <section className="bg-white pt-12 pb-20  w-6xl mx-auto">
         <div className="container mx-auto px-6 flex flex-col-reverse md:flex-row items-center w-full">
-          {/* Right Text Section */}
-          <div className="md:w-1/2 md:pl-12 flex flex-col justify-center">
-          <span className="bg-pink-100 text-pink-600 uppercase font-semibold px-3 py-1 rounded-full w-fit mb-4 tracking-widest text-xs">
-            Performance Marketing Company
-          </span>
-          <h1 className="text-xl md:text-5xl font-extrabold leading-tight text-gray-800 mb-5">
-            Global Performance Marketing Agency
-          </h1>
-           <p className='text-gray-700 mb-4 text-lg'>Are You Looking for Best Performance Marketing Agency in India That Delivers Real Results?</p>
-          <p className="text-gray-700 mb-4 text-lg">
-            At <span className="font-bold text-pink-600">Brand Orbit Media</span>, we help businesses grow with data-driven performance marketing strategies. From <span className="font-bold">Affiliate marketing</span> and <span className="font-bold">native ads</span> to <span className="font-bold">Email Marketing</span>, we focus on what matters most – ROI and scalable growth.
-          </p>
-          <p className="text-gray-700 mb-8 text-lg">
-            Whether you're looking to maximize your ROI, generate high-quality leads, or boost online sales, we create customized campaigns that convert.
-          </p>
-          <button className="ml-40 hover:bg-pink-700 hover:text-white text-pink-600 px-6 py-3 rounded-full font-semibold shadow-lg transition duration-300 text-lg w-fit mb-2">
-            Get Started
-          </button>
-        </div>
-          {/* Left Image Section */}
+          {/* Left Text Section */}
+          <div className="md:w-1/2 md:pl-12 flex flex-col justify-center ">
+            <span className="bg-pink-100 text-pink-600 uppercase font-semibold px-3 py-1 rounded-full w-fit mb-4 tracking-widest text-xs ml-26">
+              Performance Marketing Company
+            </span>
+            <h1 className="text-xl md:text-5xl font-extrabold leading-tight text-gray-800 mb-5">
+              Global Performance Marketing Agency
+            </h1>
+            <p className='text-gray-700 mb-4 text-lg'>Are You Looking for Best Performance Marketing Agency in India That Delivers Real Results?</p>
+            <p className="text-gray-700 mb-4 text-lg">
+              At <span className="font-bold text-pink-600">Brand Orbit Media</span>, we help businesses grow with data-driven performance marketing strategies. From <span className="font-bold">Affiliate marketing</span> and <span className="font-bold">native ads</span> to <span className="font-bold">Email Marketing</span>, we focus on what matters most – ROI and scalable growth.
+            </p>
+            <p className="text-gray-700 mb-8 text-lg">
+              Whether you're looking to maximize your ROI, generate high-quality leads, or boost online sales, we create customized campaigns that convert.
+            </p>
+            <button className="hover:bg-pink-700 hover:text-white text-pink-600 px-6 py-3 rounded-full font-semibold shadow-lg transition duration-600 text-lg w-fit mb-2 mx-auto cursor-pointer">
+              Get Started
+            </button>
+          </div>
+          {/* Right Image Section */}
           <div
             className="md:w-1/2 flex justify-center transition-all duration-700 ease-in-out"
             onMouseEnter={() => setHovered(true)}
@@ -43,6 +61,28 @@ const Home = () => {
           </div>
         </div>
       </section>
+
+
+        {/* Services Section */}
+      <section className="bg-gray-100 py-12 w-full mx-auto">
+        {/* upper text */}
+        <div className="w-6xl mx-auto flex flex-col justify-center items-center ">
+          <h2 className="text-4xl font-semibold text-pink-800 mb-4">
+            Our Services &amp; Features
+          </h2>
+          <p className="text-lg text-gray-700  w-130  text-center">
+            We offer wide range of services include affiliate marketing, email marketing, native ads, and social media ads, Content Marketing, Mobile Delivery and more.
+          </p>
+        </div>
+        {/* Services cards */}
+        <div className="max-w-6xl w-full mx-auto flex justify-center items-center gap-x-8 mt-8">
+                { itemInfo.map((item,index) => (
+                  <ServicesCards key={index} item={item} />
+                ))}
+        </div>
+      </section>
+
+      
     </div>
   )
 }
