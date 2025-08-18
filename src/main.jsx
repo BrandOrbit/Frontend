5324251685024e1f746fde32f850769ab6d34e9b
BrandOrbit <support@brandorbitmedia.in>
2025-08-18 20:25:49 +0530

    added initial routing setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 import App from './App.jsx'
-
+import { RouterProvider } from 'react-router-dom'
+   
+const router = createBrowserRouter([ 
+     {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '',
+          element: <Home />
+        }
+      ]
+     }
+ ])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
