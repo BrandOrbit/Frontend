8895f0ab30e4037d0e8ab0b1241cb1c6de3c0dd6
BrandOrbit <support@brandorbitmedia.in>
2025-08-19 15:01:02 +0530

    Fixed footer

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -8,8 +8,10 @@ const Layout = () => {
   return (
     <>
          <Header/>
+      <main className="flex-1 overflow-y-auto pb-16 h-125">
          <Outlet/>
-         <Footer/>
+      </main>
+      <Footer/>
     </>
   )
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 export default function Footer() {
     return (
-        <footer className="bg-white border-y">
+        <footer className="bg-white border-y fixed top-142 left-0 right-0 z-10">
             <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
                         <Link to="/" className="flex items-center">
                             <img
-                                src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+                                src="/logo.png"
                                 className="mr-3 h-16"
                                 alt="Logo"
                             />
@@ -68,7 +68,7 @@ export default function Footer() {
                         </div>
                     </div>
                 </div>
-                <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+                {/* <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
                         © 2023
@@ -155,7 +155,7 @@ export default function Footer() {
                             <span className="sr-only">Dribbble account</span>
                         </Link>
                     </div>
-                </div>
+                </div> */}
             </div>
         </footer>
     );
